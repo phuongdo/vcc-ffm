fd9586d6f168927f1860abea9d8994ea2956d80e
pdoviet <phuongdoviet@admicro.vn>
2016-07-25 14:51:16 +0700

    New changelist

--- demo/plot_roc.py
+++ demo/plot_roc.py
@@ -1,16 +1,21 @@
 from sklearn.metrics import roc_curve
 from sklearn.metrics import auc
 #import matplotlib.pyplot as plt
-import csv
+#import csv
 
 # load csv file
 labels = []
-with open('/storage/phuongdv/vc-data/te.csv') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=' ')
-    for row in reader:
-        labels.append(int(row["Label"]))
+# with open('/storage/phuongdv/vc-data/te.csv') as csvfile:
+#     reader = csv.DictReader(csvfile, delimiter=' ')
+#     for row in reader:
+#         labels.append(int(row["Label"]))
 
-lines = [line.rstrip('\n') for line in open('/storage/phuongdv/vcc-ffm/te.out')]
+with open('/storage/phuongdv/vcc-ffm/te.ffm') as csvfile:
+    for line in csvfile:
+        row = line.split(' ')
+        labels.append(int(row[0]))
+
+lines = [line.rstrip('\n') for line in open('/storage/phuongdv/vcc-ffm/te.out.cal')]
 scores = []
 for line in lines:
     scores.append(float(line))
