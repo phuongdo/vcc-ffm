e69f54bb1ab1bbf7f2cf37d431410e711c7227b9
pdoviet <phuongdoviet@admicro.vn>
2016-07-30 08:52:28 +0700

    New changelist

--- demo/plot_roc.py
+++ demo/plot_roc.py
@@ -1,28 +1,40 @@
+import sys
 from sklearn.metrics import roc_curve
 from sklearn.metrics import auc
-#import matplotlib.pyplot as plt
-#import csv
-
-# load csv file
-labels = []
-# with open('/storage/phuongdv/vc-data/te.csv') as csvfile:
-#     reader = csv.DictReader(csvfile, delimiter=' ')
-#     for row in reader:
-#         labels.append(int(row["Label"]))
-
-with open('/storage/phuongdv/vcc-ffm/te.ffm') as csvfile:
-    for line in csvfile:
-        row = line.split(' ')
-        labels.append(int(row[0]))
-
-lines = [line.rstrip('\n') for line in open('/storage/phuongdv/vcc-ffm/te.out.cal')]
-scores = []
-for line in lines:
-    scores.append(float(line))
 
 
-# Compute fpr, tpr, thresholds and roc auc
-fpr, tpr, thresholds = roc_curve(labels, scores)
-roc_auc = auc(fpr, tpr)
+# import matplotlib.pyplot as plt
+# import csv
 
-print(roc_auc)
+
+
+
+def main(argv):
+    label_file = argv[0]
+    score_file = argv[1]
+    # load csv file
+    labels = []
+    # with open('/storage/phuongdv/vc-data/te.csv') as csvfile:
+    #     reader = csv.DictReader(csvfile, delimiter=' ')
+    #     for row in reader:
+    #         labels.append(int(row["Label"]))
+
+    with open(label_file) as csvfile:
+        for line in csvfile:
+            row = line.split(' ')
+            labels.append(int(row[0]))
+
+    lines = [line.rstrip('\n') for line in open(score_file)]
+    scores = []
+    for line in lines:
+        scores.append(float(line))
+
+    # Compute fpr, tpr, thresholds and roc auc
+    fpr, tpr, thresholds = roc_curve(labels, scores)
+    roc_auc = auc(fpr, tpr)
+
+    print("AUC : {}".format(roc_auc))
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
